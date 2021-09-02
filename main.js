3699cd04f1c8bbf93721347789c826fa31ff3edd
Aritra Rakshit <arakshit.engineer@gmail.com>
2021-09-02 21:00:01 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstWords = ['I', 'We', 'You', 'He', 'She']
+const secondWords = ['am', 'is', 'are']
+const thirdWords = ['fantastic', 'good', 'bad', 'sad']
+console.log(firstWords[Math.floor(Math.random()* firstWords.length)] + ' ' + 
+secondWords[Math.floor(Math.random()* secondWords.length)] + ' ' + 
+thirdWords[Math.floor(Math.random()* thirdWords.length)])
